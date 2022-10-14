0d5f4ac35d40b95af8ab39d5663c9564035878e3
Clickadelic <toby.hopp@gmail.com>
2022-10-15 00:38:08 +0200

    1st working prototype

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,44 @@
 const currentList = []
+const input = document.getElementById('root-input')
+const ul = document.getElementById('root-ul')
 
 class Todo {
-
-    constructor(){
-        const rootApp = document.getElementById('app')
-        const rootUL = document.createElement('ul')
-        rootApp.appendChild(rootUL)
+    // Constructor
+    constructor(currentList){
+        // ist kein setter oder getter > also ohne _ ???
+        this.currentList = currentList
+        this.addTodoListeners()
+    }
+    
+    // Add DOM Event Listeners
+    addTodoListeners(){
+        input.addEventListener('change', (event) => {
+            this.pushTodoValue(event.target.value)       
+        })
     }
 
-    renderDOM(){
-        
+    // Push value from input field to array
+    pushTodoValue(value){
+        currentList.push(value)
+        this.renderTodoList(currentList)
+        this.clearTodoField()
+    }
+
+    // Render Todo list
+    renderTodoList(currentList){
+        currentList.forEach(item => {
+            let li = document.createElement('li')
+            li.innerText = item
+            ul.appendChild(li)
+        })
+    }
+
+    clearTodoField(){
+        input.value = ''
     }
 }
- 
+
 // Load everything
 window.addEventListener('load', () => {
-    const App = new Todo()
+    const TodoList = new Todo(currentList)
 })
